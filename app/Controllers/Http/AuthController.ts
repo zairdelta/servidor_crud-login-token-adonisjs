9138fa41753f988aa6118712552791c2227ceb6a
zairdelta <86736553+zairdelta@users.noreply.github.com>
2022-03-22 20:53:59 -0600

    Update AuthController.ts

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -5,27 +5,6 @@ import User from 'App/Models/User'
 export default class AuthController {
 
    
-    public async register({request,response}:HttpContextContract){
-        const validations= await schema.create({
-            email:schema.string({},[
-               rules.email(),
-               rules.unique({table:'users',column:'email'})
-            ]),
-            password:schema.string()
-
-        })
-        const data=await request.validate({schema:validations})
-        const user=await User.create(data)
-        return response.created(user);
-    }
-
-    public async login({request,auth}:HttpContextContract){
-        const email=request.input('email')
-        const password=request.input('password')
-        const token= await auth.attempt(email,password)
-
-        return token.toJSON()
-    }
-    
+  //AQUI VA TU REGISTRO Y TU LOGIN
 }
 
